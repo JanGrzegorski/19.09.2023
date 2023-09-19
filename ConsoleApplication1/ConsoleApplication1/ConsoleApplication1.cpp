31c1e7cfc3021f2ef9ff246ff25c364a8e109b09
bliseek <kacperkempka1@gmail.com>
2023-09-19 09:49:33 +0200

    ConsoleApplication1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+void opisKalkulatora();//deklaracja funkcji nic nie zwracającej
+float dodawanie(float liczba1, float liczba2);
+float odejmowanie(float liczba1, float liczba2);
+int main()
+{
+	float liczba1, liczba2;
+	opisKalkulatora();
+	cout << "Podaj liczba1 = "; cin >> liczba1;
+	cout << "Podaj liczba2 = "; cin >> liczba2;
+	cout << "Dodawanie = " << dodawanie(liczba1, liczba2) << endl;
+
+}
+void opisKalkulatora() {
+	cout << "Jan Grzegorski:Ver 2 Kalkulatora" << endl;
+
+}
+float dodawanie(float liczba1, float liczba2)
+{
+	return liczba1 + liczba2;
+}
